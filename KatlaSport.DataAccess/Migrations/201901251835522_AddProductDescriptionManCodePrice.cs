59d449074053cc0b016ed3a39df9b2193fa5a105
Kirill Dimidyuk <kirill.dimidyuk@gmail.com>
2019-01-25 22:17:50 +0300

    Add Description, ManufacturerCode and Price property to CatalogueProduct and UpdateProductRequest model. Create a new migration AddProductDescriptionManCodePrice. Add new rules to UpdateProductRequestValidator.

---
+++ KatlaSport.DataAccess/Migrations/201901251835522_AddProductDescriptionManCodePrice.cs
@@ -0,0 +1,15 @@
+namespace KatlaSport.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AddProductDescriptionManCodePrice : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
